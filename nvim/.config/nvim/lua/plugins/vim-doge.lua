195de9d7feac0e2f1b9819f1ba0c3c0d67127272
nategphillips <30729007+nategphillips@users.noreply.github.com>
2025-08-10 18:36:51 -0500

    update nvim: improve doge config

--- nvim/.config/nvim/lua/plugins/vim-doge.lua
+++ nvim/.config/nvim/lua/plugins/vim-doge.lua
@@ -3,5 +3,11 @@ return {
     "kkoomen/vim-doge",
     -- load after reading a buffer into memory
     event = "BufRead",
-    vim.keymap.set('n', '<leader>dg', '<CMD>DogeGenerate google<CR>')
+    init = function()
+        -- disable default keymaps (they conflict)
+        vim.g.doge_enable_mappings = false
+        -- set default Python docstring style
+        vim.g.doge_doc_standard_python = 'google'
+    end,
+    vim.keymap.set('n', '<leader>dg', '<CMD>DogeGenerate<CR>'),
 }
