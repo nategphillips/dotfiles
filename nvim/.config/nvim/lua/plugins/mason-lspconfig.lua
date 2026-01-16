45f374d92e7e2fb40b1d80e7e0802d9bbd815311
nategphillips <30729007+nategphillips@users.noreply.github.com>
2026-01-16 11:08:53 -0600

    nvim: remove rust lsp, i don't use it

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -4,16 +4,22 @@ return {
     -- load before reading a buffer into memory
     event = "BufReadPre",
     opts = {
-        -- lua_ls for basic lua support because why not
-        -- basedpyright is an improved fork of pyright for static type checking in python
-        -- ruff is installed here for linting highlights while editing, but it's also called from
-        -- the cli via conform, not sure if this is ideal but whatever
-        -- texlab for latex autocompletion/snippets
-        -- ltex_plus is a more maintained fork of ltex that provides grammar and spell checking
-        -- julials is broken(!) when installed with mason right now, so it's installed manually
-        --      - see https://github.com/mason-org/mason-lspconfig.nvim/issues/582
-        --      - and https://discourse.julialang.org/t/neovim-languageserver-jl-crashing-again/130273/3
-        ensure_installed = { "lua_ls", "basedpyright", "ruff", "texlab", "ltex_plus", "rust_analyzer" },
+        ensure_installed = {
+            -- lua_ls for basic lua support because why not
+            "lua_ls",
+            -- basedpyright is an improved fork of pyright for static type checking in python
+            "basedpyright",
+            -- ruff is installed here for linting highlights while editing, but it's also called
+            -- from the cli via conform, not sure if this is ideal but whatever
+            "ruff",
+            -- texlab for latex autocompletion/snippets
+            "texlab",
+            -- ltex_plus is a more maintained fork of ltex that provides grammar and spell checking
+            "ltex_plus",
+            -- julials is broken(!) when installed with mason right now, so it's installed manually
+            --      - see https://github.com/mason-org/mason-lspconfig.nvim/issues/582
+            --      - and https://discourse.julialang.org/t/neovim-languageserver-jl-crashing-again/130273/3
+        },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
