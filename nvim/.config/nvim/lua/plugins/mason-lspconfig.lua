847de62171795b9fe4ac27b1ca5aca148c55f172
nategphillips <30729007+nategphillips@users.noreply.github.com>
2026-08-24 18:30:43 -0600

    nvim: add julials back to mason since it works now

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -16,9 +16,10 @@ return {
             "texlab",
             -- ltex_plus is a more maintained fork of ltex that provides grammar and spell checking
             "ltex_plus",
-            -- julials is broken(!) when installed with mason right now, so it's installed manually
+            -- julials is no longer broken with mason, yay!
             --      - see https://github.com/mason-org/mason-lspconfig.nvim/issues/582
             --      - and https://discourse.julialang.org/t/neovim-languageserver-jl-crashing-again/130273/3
+            "julials",
         },
     },
     dependencies = {
